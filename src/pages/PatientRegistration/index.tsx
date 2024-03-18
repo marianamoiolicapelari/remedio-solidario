6c8f8b4463be0b3e58c631a4460badf2eabe2047
Mariana Capelari <marianamoioli@gmail.com>
2024-03-18 17:21:42 -0300

    Botão editar paciente ok

--- src/pages/PatientRegistration/index.tsx
+++ src/pages/PatientRegistration/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Table, Thead, Tbody, Tr, Th, Td, TableContainer, Button, Flex, } from '@chakra-ui/react'
+import { Box, Text, Table, Thead, Tbody, Tr, Th, Td, TableContainer, Button, Flex, Tooltip } from '@chakra-ui/react'
 import { Footer, Header } from '../../components'
 import { MdOutlineEdit, MdDeleteOutline } from 'react-icons/md'
 import { api } from '../../services/api'
@@ -27,11 +27,21 @@ const PatientRegistration = () => {
     fetchData()
   }, [])
 
+  const handleDeletePatient = async (id: number) => {
+    try {
+      await api.delete(`/patients/${id}`)
+      setPatients(patients.filter(patient => patient.id !== id))
+      console.log(patients)
+    } catch (error) {
+      console.error('Erro ao excluir paciente:', error)
+    }
+  }
+
   return (
     <>
       <Header />
       <Box height='calc(100vh - 115px)' p={8}>
-        <Text fontWeight="bold" fontSize='xl' mb={5}>Lista de pacientes cadastrados</Text>
+        <Text fontWeight="bold" fontSize='xl' mb={8}>Pacientes cadastrados</Text>
         <TableContainer>
           <Table variant='simple' colorScheme='blue'>
             <Thead>
@@ -50,8 +60,16 @@ const PatientRegistration = () => {
                   <Td >{patient.address}</Td>
                   <Td>
                     <Flex justify={'end'}>
-                    <Button mr={2}><MdOutlineEdit /></Button>
-                    <Button><MdDeleteOutline /></Button>
+                      <Tooltip label='Editar' fontSize='md' placement='top'>
+                        <Button mr={2}>
+                          <MdOutlineEdit />
+                        </Button>
+                      </Tooltip>
+                      <Tooltip label='Excluir' fontSize='md' placement='top'>
+                        <Button mr={2}>
+                          <MdDeleteOutline onClick={() => handleDeletePatient(patient.id)}/>
+                        </Button>
+                      </Tooltip>
                     </Flex>
                   </Td>
                 </Tr>
