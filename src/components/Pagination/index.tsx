43ffbdad9eac53a53ebcac1d50253580cf0e5bd7
Mariana Capelari <marianamoioli@gmail.com>
2024-03-24 20:11:19 -0300

    Paginação finalizada

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import { Button, Flex, Text } from '@chakra-ui/react'
 
 interface PaginationProps {
   currentPage: number
@@ -10,14 +10,14 @@ interface PaginationProps {
 const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
   const [displayedPages, setDisplayedPages] = useState<number[]>([])
 
-
   React.useEffect(() => {
     const pages: number[] = []
     const totalDisplayedPages = 5
 
     let startPage = currentPage - Math.floor(totalDisplayedPages / 2)
     startPage = Math.max(startPage, 1)
-    startPage = Math.min(startPage, Math.max(1, totalPages - totalDisplayedPages + 1))
+    const maxStartPage = Math.max(1, totalPages - totalDisplayedPages + 1)
+    startPage = Math.min(startPage, maxStartPage)
 
     for (let i = 0; i < totalDisplayedPages && startPage + i <= totalPages; i++) {
       pages.push(startPage + i)
@@ -26,20 +26,22 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
     setDisplayedPages(pages)
   }, [currentPage, totalPages])
 
-  return (
-    <Flex mt={8} direction='column' alignItems={'center'}>
+  const firstPage = 1
+  const lastPage = totalPages
 
+  return (
+    <Flex mt={4} direction="column" alignItems="center">
       <Flex>
-        <Flex>
-          <Button
-            onClick={() => onPageChange(currentPage - 1)}
-            disabled={currentPage === 1}
-            mr={2}
-          >
-            Anterior
-          </Button>
-        </Flex>
-        {displayedPages.map(page => (
+        <Button onClick={() => onPageChange(firstPage)} disabled={currentPage === firstPage} mr={2}>
+          &lt;&lt;
+        </Button>
+        <Button
+          onClick={() => onPageChange(Math.max(currentPage - 1, 1))}
+          disabled={currentPage === firstPage}
+          mr={2}>
+          Anterior
+        </Button>
+        {displayedPages.map((page) => (
           <Button
             key={page}
             onClick={() => onPageChange(page)}
@@ -50,22 +52,24 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
             {page}
           </Button>
         ))}
-        <Box>
-          <Button
-            onClick={() => onPageChange(currentPage + 1)}
-            disabled={currentPage === totalPages}
-            ml={2}
-          >
-            Próximo
-          </Button>
-        </Box>
-      </Flex>
-      <Text ml={2} mt={1}>
-        Página {currentPage} de {totalPages}
-      </Text>
-
+        {totalPages > 5 && (
+          <>
+            {currentPage <= totalPages - 3 && <Text>...</Text>}
+            <Button onClick={() => onPageChange(lastPage)}>{lastPage}</Button>
+          </>
+        )}
+        <Button
+          onClick={() => onPageChange(Math.min(currentPage + 1, totalPages))}
+          disabled={currentPage === lastPage}
+          ml={2}>
+          Próximo
+        </Button>
+        <Button onClick={() => onPageChange(lastPage)} disabled={currentPage === lastPage} ml={2}>
+          &gt;&gt;
+        </Button>
+      </Flex>  
     </Flex>
-  );
-};
+  )
+}
 
-export default Pagination;
+export default Pagination
